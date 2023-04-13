1040a652d479b0d26dc2b59b4ad6077f029ca078
Francisco Riveros <francisco@Franciscos-MacBook-Air.local>
2023-04-12 21:59:19 -0400

    termino de formularios por template

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,5 +1,14 @@
 import { Component } from '@angular/core';
 
+interface Persona{
+  nombre:string;
+  favoritos: Favorito[];
+}
+interface Favorito{
+  id:number;
+  nombre:string
+}
+
 @Component({
   selector: 'app-dinamicos',
   templateUrl: './dinamicos.component.html',
@@ -7,4 +16,29 @@ import { Component } from '@angular/core';
 })
 export class DinamicosComponent {
 
+  nuevoJuego:string ='';
+
+  persona : Persona = {
+    nombre: 'Francisco',
+    favoritos: [
+      { id:1,nombre:'Metal Gear' },
+      { id:2,nombre:'Mario Bross' },
+    ]
+  }
+
+  guardar(){
+    console.log("hola");
+  }
+  agregarJuego(){
+    const nuevoFavorito:Favorito = {
+      id: this.persona.favoritos.length + 1,
+      nombre: this.nuevoJuego
+    }
+    this.persona.favoritos.push({...nuevoFavorito});
+    this.nuevoJuego = '';
+  }
+
+  eliminar(index:number){
+    this.persona.favoritos.splice(index,1);
+  }
 }
--- src/app/template/switches/switches.component.ts
+++ src/app/template/switches/switches.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class SwitchesComponent {
 
+  persona = {
+    genero: 'F',
+    notificaciones: false
+  }
+
+  terminosYCondiciones = false;
 }
